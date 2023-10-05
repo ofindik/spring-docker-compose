15f4fe12cba3f838b67d4902ade8622ff625cca4
osmanf <osmanfindik@gmail.com>
2023-10-05 22:34:30 +0300

    Debugging the JDBC connection

--- src/main/java/dev/osmanfindik/dockercompose/DockerComposeApplication.java
+++ src/main/java/dev/osmanfindik/dockercompose/DockerComposeApplication.java
@@ -1,13 +1,29 @@
 package dev.osmanfindik.dockercompose;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DockerComposeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerComposeApplication.class, args);
+	public static void main (String[] args) {
+		SpringApplication.run (DockerComposeApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner (JdbcConnectionDetails jdbc) {
+		return args -> {
+			var details = STR. """
+			class: \{ jdbc.getClass ().getName () }
+			JDBC URL: \{ jdbc.getJdbcUrl () }
+			Username: \{ jdbc.getUsername () }
+			Password: \{ jdbc.getPassword () }
+					""" ;
+
+			System.out.println ("Details: " + details);
+		};
+	}
 }
